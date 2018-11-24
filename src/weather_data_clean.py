7ea4007081b69e2d6fa6f131460eca4c4a9a759e
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-11-23 18:11:48 -0800

    Add new weather_data_clean.py file containing code from the ASOS data exploration notebook

---
+++ src/weather_data_clean.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+def clean_weather_data(filename):
+    """Take ASOS weather data file for Stampede pass and clean it ready for input to model.
+    Input: txt file
+    Output: pandas dataframe
+    """
+    data = pd.read_csv(filename)
+
+    # Rename two of the columns
+    data.rename(columns={'valid':'date', 'tmpf':'temp'}, inplace=True)
+
+    # Remove the few rows that have a null value for temp
+    data = data[~data.temp.eq('M')]
+
+    # Remove spaces from column names
+    data.rename(columns=lambda x: x.replace(' ', ''), inplace=True)
+
+    # Create a date series to be used in the clean dataframe
+    date = pd.to_datetime(data['date'])
+
+    # Create a temp series to be used in the clean dataframe
+    temp = data['temp'].apply(float)
+
+    # Cast the null value M to zero to enable create of the raw precipitation series cast to floats
+    data.p01i[data.p01i == 'M'] = 0
+    raw_precipitation = data['p01i'].apply(float)
+
+    # Create a precipitation series to be used in the clean dataframe
+    precipitation = raw_precipitation.apply(lambda x: True if (x > 0) else False)
+
+    # Convert sky coverage data to clear or cloudy and create an overcast series to be used in the clean dataframe
+    sky_elements = ['skyc1', 'skyc2', 'skyc3']
+    data.skyc1 = data.skyc1.astype(str)
+    data.skyc2 = data.skyc2.astype(str)
+    data.skyc3 = data.skyc3.astype(str)
+    sky_agg = data[sky_elements].values.tolist()
+    sky_reduce = [['overcast' if (('BKN' in element) or ('OVC' in element) or ('VV' in element)) else 'clear'
+                    for element in row] for row in sky_agg]
+    overcast = pd.Series([True if 'overcast' in row else False for row in sky_reduce])
+
+    # Cast the null value 'M' to 10.00 to enable the creation of a poor visibility series
+    data.vsby[data.vsby == 'M'] = 10.00
+    raw_visibility = data['vsby'].apply(float)
+    poor_visibility = pd.Series([True if value < 0.50 else False for value in raw_visibility])
+
+    # Cast the null value 'M' to 0 to enable the creation of a windy series
+    data.sknt[data.sknt == 'M'] = 0.00
+    data.gust[data.gust == 'M'] = 0.00
+    wind_speed = data['sknt'].apply(float)
+    gust_speed = data['gust'].apply(float)
+    wind_df = pd.concat([wind_speed, gust_speed], axis=1)
+    # Finally apply the function f to enable the creation of the windy column
+    windy = wind_df.apply(f, axis=1)
+
+    """Create the cleaned dataframe by concatenating the date, temp, precipitation, overcast, poor_visibility
+    and windy series"""
+    cleaned_df = pd.concat([date, temp, precipitation, overcast, poor_visibility, windy], axis=1)
+    cleaned_df.columns = ['date', 'temp', 'precipitation', 'overcast', 'poor_visibility', 'windy']
+    return cleaned_df
+
+def f(row):
+    """Function to be able to create the windy series with windy being true if wind speed is above 10 knots
+    or gust speed is above 20 knots"""
+    if row['sknt'] >= 10.00:
+        val = True
+    elif row['gust'] >= 20.00:
+        val = True
+    else:
+        val = False
+    return val
+
+
