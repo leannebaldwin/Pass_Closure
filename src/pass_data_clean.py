9f7fc80e628d67cbd7630dac3335411c408e9487
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-11-26 14:16:26 -0800

    Add required changes to have start and end times in dataframe rather than date and delay amount

--- src/pass_data_clean.py
+++ src/pass_data_clean.py
@@ -13,8 +13,9 @@ def clean_pass_data(filename):
     #drop unnecessary secondary incident columns
     data.drop(data.columns[[1,8]], axis=1, inplace=True)
 
-    #rename 'Incident start times...' column to date
-    data.rename(columns={'INCIDENT START TIMES FOR EACH DIRECTION':'date'}, inplace=True)
+    #rename 'Incident...' columns to start_time and end_time 
+    data.rename(columns={'INCIDENT START TIMES FOR EACH DIRECTION':'start_time'}, inplace=True)
+    data.rename(columns={'INCIDENT END TIMES - DIRECTIONAL':'end_time'}, inplace=True)
 
     #use only dates from 2007-01-01 to match with available weather and traffic volume data
     df = data[(data['date'] > '2006-12-31')]
@@ -31,17 +32,17 @@ def clean_pass_data(filename):
     #create a snow pandas series with True if weather description contains sn, false otherwise
     snow = df.WEATHER.str.contains('sn', case=False, na=False, regex=True)
 
-    #create pandas series for date and delay
-    date = pd.to_datetime(df['date'])
-    delay = df['delay']
+    #create pandas series for start and end times
+    start_time = pd.to_datetime(df['start_time'])
+    end_time = pd.to_datetime(df['end_time'])
 
     #ensure that all the pandas series created have the same index
-    westbound.index = date.index
-    snow.index = date.index
-    delay.index = date.index
+    westbound.index = start_time.index
+    snow.index = start_time.index
+    end_time.index = start_time.index
 
     #create cleaned df with the series created
-    cleaned_df = pd.concat([date, delay, westbound, snow], axis=1)
+    cleaned_df = pd.concat([start_time, end_time, westbound, snow], axis=1)
 
     #rename columns
     cleaned_df.rename(columns={0:'westbound', 'WEATHER':'snow'}, inplace=True)
