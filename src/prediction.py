b092007f1455d297e7ca5164fcc8a83346550100
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-04 11:51:46 -0800

    Add functions to use pickled file to get predictions on scraped weather data stored in mongo db

--- src/prediction.py
+++ src/prediction.py
@@ -1,2 +1,23 @@
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+import pickle
+
+with open('../data/pickled_pipe.pkl', 'rb') as f:
+    pipe = pickle.load(f)
+
+def get_predictions(df):
+    """function to use pickled pipeline to get predictions on new scraped weather forecast data
+    Input: pandas dataframe
+    Output: numpy array of predictions
+    """
+    forecast_probs = pipe.predict(df)
+    return forecast_probs
+
+def get_one_prediction(row: dict) -> float:
+    """make a prediction for a single event, i.e. a single row from Mongo DB where scraped weather data is stored
+    Input: dict
+    Output: float
+    """
+    df = pd.DataFrame([row])
+    predictions = get_predictions(df)
+    return predictions[0, 1]
