20542d76fb7ebd8cf243b7ba756b5ec4120f3fdb
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-05 14:05:12 -0800

    Add notebook to re-pickle the pipeline

--- src/combine_data.py
+++ src/combine_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pass_data_clean import clean_pass_data
 
-pass_closure_df = clean_pass_data('../data/Cumulative_Snoqualmie_Pass_Delay_Closures_1992_2018.xlsx')
+pass_closure_df = clean_pass_data('../data_exploration/Cumulative_Snoqualmie_Pass_Delay_Closures_1992_2018.xlsx')
 
 def get_pass_closure(date_time):
     """take a date_time and check if it is between the start and end times of a closure event
