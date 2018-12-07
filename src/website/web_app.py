ab45911158de015dc2a97424408ec25e247c8f6f
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-06 16:44:48 -0800

    Add plot to website

--- src/website/web_app.py
+++ src/website/web_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from pipeline_classes import Featurizer
 import pandas as pd
+import matplotlib.pyplot as plt
 import prediction
 
 app = Flask(__name__, static_url_path="")
@@ -13,7 +14,7 @@ mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
 def render():
-    return render_template('index.html', table = get_pred())
+    return render_template('index.html', table = output.to_html(index=False), plot = plt.show())
 
 def get_pred():
     """Get the predictions and data to display"""
@@ -26,6 +27,16 @@ def get_pred():
     df['probabilities'] = predictions[:,1]
     df = df.round(2)
     output = df.loc[:, ['date', 'probabilities']]
-    return output.to_html(index=False)
+    return output
+
+output = get_pred()
+
+ax = output[['probabilities']].plot(kind='bar', title ="Probabilities", figsize=(15, 10), legend=True, fontsize=12)
+ax.set_xlabel("Date", fontsize=12)
+ax.set_ylabel("Probabilities", fontsize=12)
+ax.set_xticklabels(output['date'], rotation='vertical')
+
+
+
 
 
