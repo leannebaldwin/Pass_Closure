84409d4e36ec3222b165800593e9b4fbeb313b0f
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-07 11:08:27 -0800

    Add code for another potential solution to get my plot to display on website

--- src/website/web_app.py
+++ src/website/web_app.py
@@ -3,9 +3,8 @@ from flask_pymongo import PyMongo
 from pipeline_classes import Featurizer
 import pandas as pd
 import io
+import base64
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 import prediction
 
 app = Flask(__name__, static_url_path="")
@@ -17,7 +16,7 @@ mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
 def render():
-    return render_template('index.html', table = output.to_html(index=False), plot = plt.show())
+    return render_template('index.html', table = output.to_html(index=False))
 
 def get_pred():
     """Get the predictions and data to display"""
@@ -34,23 +33,18 @@ def get_pred():
 
 output = get_pred()
 
-@app.route('/plot.png')
-def plot_png():
-    '''Generate image for display on web page'''
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
+@app.route('/plot')
+def build_plot():
     '''Create bar chart to display a visual representation of predicted probabilities'''
-    fig = Figure()
+    img = io.BytesIO()
     ax = output[['probabilities']].plot(kind='bar', title ="Probabilities", figsize=(15, 10), legend=True, fontsize=12)
     ax.set_xlabel("Date", fontsize=12)
     ax.set_ylabel("Probabilities", fontsize=12)
     ax.set_xticklabels(output['date'], rotation='vertical')
-    ax.plot()
-    return fig
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 
 
