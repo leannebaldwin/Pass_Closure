46716e31264ca0e3c0a75d53a7b91c2167c07c14
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-11 10:59:31 -0800

    Add import make_response

--- src/website/web_app.py
+++ src/website/web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, make_response
 from flask_pymongo import PyMongo
 from pipeline_classes import Featurizer
 import pandas as pd
