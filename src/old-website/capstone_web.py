172ba2b07c1836dd46dadbc01f52bb59ac48c89c
Leanne Baldwin <leanne@reefbreak.co.uk>
2018-12-13 14:30:20 -0800

    Remove old website docs

--- src/old-website/capstone_web.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__, static_url_path='')
-
-@app.route('/')
-def render():
-    return render_template('index.html', title = 'Snoqualmie Pass Closure Forecaster')
